f3f249fc9b27d0bcef9674f51e30a9a90fd83cff
Ryan Hasbury <rhasbury@gmail.com>
2016-04-09 08:37:01 -0400

    Added error logging and initrd script

--- Rpi/GasSensorLogger.py
+++ Rpi/GasSensorLogger.py
@@ -1,6 +1,6 @@
 import os
 import logging
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', filename='./gassensor.log', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', filename='/home/pi/GasSensorLogger/Rpi/gassensor.log', level=logging.DEBUG)
 import datetime
 import pymysql.cursors
 import time
@@ -12,7 +12,7 @@ import signal
 import serial
 
 currentLocation = 'basement'
-loginterval = 600 # in seconds
+loginterval = 300 # in seconds
 
 serialPort = '/dev/ttyUSB0'
 
@@ -53,34 +53,36 @@ class GasPoller(threading.Thread):
     def run(self):
         global gpsd
         global gpsp
-        
-        
-              
+                              
         ser = serial.Serial(serialPort, 9600, bytesize=8, parity='N', stopbits=1, timeout=1, rtscts=False, dsrdtr=False)
         #ser.setRTS(0) 
         time.sleep(3) # creating connection will reset arduino, need to wait for reset complete. 
         while gpsp.running:      
-            time.sleep(0.1)
-            ser.flushInput()
+            try:
+                time.sleep(0.1)
+                ser.flushInput()
+                
+                ser.write(b'get_a0;')       
+                time.sleep(0.1)
+                a0 = ser.readline()            
+                ser.write(b'get_a1;')
+                time.sleep(0.1)
+                a1 = ser.readline()
+                ser.write(b'get_a2;')
+                time.sleep(0.1)
+                a2 = ser.readline()
+                
+                print("a0: {}  a1: {}  a2:  {}".format(int(a0), int(a1), int(a2)))
             
-            ser.write(b'get_a0;')       
-            time.sleep(0.1)
-            a0 = ser.readline()            
-            ser.write(b'get_a1;')
-            time.sleep(0.1)
-            a1 = ser.readline()
-            ser.write(b'get_a2;')
-            time.sleep(0.1)
-            a2 = ser.readline()
-            
-            print("a0: {}  a1: {}  a2:  {}".format(int(a0), int(a1), int(a2)))
-        
-            logGaslineDB("MQ-135", currentLocation, int(a0))            
-            logGaslineDB("MQ-5", currentLocation, int(a1))
-            logGaslineDB("MQ-9", currentLocation, int(a2))
+                logGaslineDB("MQ-135", currentLocation, int(a0))            
+                logGaslineDB("MQ-5", currentLocation, int(a1))
+                logGaslineDB("MQ-9", currentLocation, int(a2))
             
                 
-            time.sleep(loginterval)
+                time.sleep(loginterval)
+            except:
+                 logging.error("Exception in main logging loop ", exc_info=True)
+            
         
         ser.close()
         print("ended")
