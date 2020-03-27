156e7aee7c75bd5a071e69146fbbb8568a9b1a90
Ryan Hasbury <rhasbury@gmail.com>
2020-03-27 00:11:43 -0400

    Added CO2 logger

--- Arduino/GasRead/GasRead.ino
+++ Arduino/GasRead/GasRead.ino
@@ -2,6 +2,7 @@
    Suitable for detecting of LPG, i-butane, propane, methane ,alcohol, Hydrogen or smoke
    More info: http://www.ardumotive.com/how-to-use-mq2-gas-sensor-en.html
    Dev: Michalis Vasilakis // Date: 11/6/2015 // www.ardumotive.com                     */
+#include "Adafruit_CCS811.h"
 
 const int gasPin = A0; //GAS sensor output pin to Arduino analog A0 pin
 const int gasPin1 = A1; //GAS sensor output pin to Arduino analog A0 pin
@@ -12,6 +13,8 @@ const int ledPower = 2;
 const int delayTime=280;
 const int dustPin=A3;
 const int delayTime2=40;
+
+Adafruit_CCS811 ccs;
 
  /*
  Sharp pin 1 (V-LED)   => 5V (connected to 150ohm resister)
@@ -29,8 +32,16 @@ boolean stringComplete = false;  // whether the string is complete
 void setup()
 {
   pinMode(ledPower,OUTPUT);
+  pinMode(PD4, OUTPUT);
+  digitalWrite(PD4,LOW);
   Serial.begin(9600); //Initialize serial port - 9600 bps
+  Serial.println(SDA);
+  Serial.println(SCL);
    // reserve 200 bytes for the inputString:
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");    
+  }
+      
   inputString.reserve(200);
 }
 
@@ -51,6 +62,25 @@ void loop()
     if(inputString.indexOf("get_a2") >= 0){
       Serial.println(analogRead(gasPin2));    
     }
+    if(inputString.indexOf("get_co2") >= 0){
+      if(!ccs.readData()){      
+        Serial.println(ccs.geteCO2());              
+      }
+      else{
+        Serial.println("ERROR!");      
+      }
+    }
+
+    if(inputString.indexOf("get_tvoc") >= 0){
+      if(!ccs.readData()){                    
+        Serial.println(ccs.getTVOC());
+      }
+      else{
+        Serial.println("ERROR!");      
+      }
+      
+    }
+
     
     if(inputString.indexOf("get_dust") >= 0){
       
